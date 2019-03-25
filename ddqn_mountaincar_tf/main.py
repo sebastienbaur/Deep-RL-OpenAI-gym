466b79b296df37ef4f148e8fad87e2d70ef34031
Sebastien <sebastien.baur@hotmail.fr>
2019-03-25 11:10:35 +0100

    adding tensorflow version

---
+++ ddqn_mountaincar_tf/main.py
@@ -0,0 +1,190 @@
+"""
+*********************************************
+******   This is a copy of the README  ******
+*********************************************
+
+DDQN on OpenAI's MountainCar problem
+
+- Implementation of DDQN : https://arxiv.org/abs/1509.06461
+- Works with the OpenAI gym environment (MountainCar-v0)
+- Use OpenAI PER code (https://github.com/openai/baselines/blob/master/baselines/deepq/replay_buffer.py)
+- Trying different choices of hyperparameters
+
+
+Hyperparameters that work well:
+BATCH_SIZE = 64
+TAU = 100  # the update frequency of the target network
+GAMMA = .999  # discount factor
+N_RANDOM = 40000.  # the number of steps it takes to go from eps=1. to eps=0.05 (exponentially decreasing, loses 99% of the .95 in 40000 steps)
+N_EPISODES = 1500
+MAX_SIZE_BUFFER = 20000  # the maximum number of transitions in the buffer
+T_START_LEARNING = BATCH_SIZE * 10  # the minimum number of transitions to be seen in the buffer before training
+ALPHA = .6  # default OpenAI
+BETA = .4  # default OpenAI
+PER_EPS = 1e-6
+RMSProp, lr 2e-4, momentum 5e-2
+original rewards
+
+
+Hyperparameters whose influence should be tested:
+- OPTIMIZER: RMSProp with small momentum >>> Adam. Note that dimishing one of Adam's betas may have the same effect. This is because the objective is non-stationnary, so momentum hurts training
+- UPDATE FREQUENCY: Reducing from 10000 to 200 made A HUGE DIFFERENCE (with 10000, there was no learning).
+                    Reducing it to 100 makes the performance a bit better still (faster learning)
+                    Reducing it to 10 makes it about the same
+- GAMMA: .999 is better than .99.
+         With the former, training is way faster. It is probably because the positive rewards are delayed in time, so having a higher GAMMA gives them more importance
+         It is essential to learn a successful policy
+- THE REWARD FUNCTION: it is better to have a dense reward signal, as it helps the agent towards the objective. Note that maximizing the cumulative discounted reward should still be
+                       aligned with the objective (i.e. there shouldn't be any way of maximizing it without achieving the goal)
+                       NOTE: IS IT REALLT THIS THAT MAKES THE AGENT LEARN SOMETHING, OR RATHER THE UPDATE FREQUENCY OF THE TARGET NETWORK
+- DENSE REWARDS: The original rewards aren't actually sparse (-1 at each time step), but there is not any truly useful signal before the car reaches the flag.
+                 I thought that this signal didn't help and modified it to add a positive signal depending on how close the car gets to the flag.
+                 It turns out that the original reward scheme is better (way faster training). It is probably because when tryng an action that doesnt help reaching the flag,
+                 its values is decreased (the reward was -1), so another one will be tried next time, which may lead to find the optimal solution.
+
+
+In conclusion, two parameters are REALLY USEFUL:
+- using a target network to have a more stationary training objective (DDQN vs DQN)
+- the update frequency of the target (updating not too frequently (more than 1 which is the DQN case), more like every episode, like 100 or 200)
+- using an optimizer with little momentum
+
+
+As the rewards are always negative in the normal reward scheme, it may sound sensible to constrain the output of the Q network to be always negative (by outputting -F.relu(...)) for example
+As it turns out, doing this makes learning much harder. Most values tend to be zero (Q(s,a) = 0 \forall s,a), and nothing happens. The reason is rather obscure to me
+
+
+TODO:
+---> visualiser l'erreur V(s) - Q(s,a) a intervalles reguliers
+"""
+
+from copy import deepcopy
+from itertools import product
+
+import gym
+import numpy as np
+import torch
+from tensorboardX import SummaryWriter
+
+from ddqn_mountaincar.per import PrioritizedReplayBuffer
+from ddqn_mountaincar.utils import DQN, update_eval_network, build_target, huber, update_reward, tensorboard
+from dqn_mountaincar.utils import create_exp_dir
+
+# Hyperparams
+BATCH_SIZE = 64
+TAU = 100  # the update frequency of the target network
+GAMMA = .999  # discount factor
+N_RANDOM = 40000.  # the number of steps it takes to go from eps=1. to eps=0.05 (exponentially decreasing, loses 99% of the .95 in 40000 steps)
+N_EPISODES = 1500
+MAX_SIZE_BUFFER = 20000  # the maximum number of transitions in the buffer
+T_START_LEARNING = BATCH_SIZE * 10  # the minimum number of transitions to be seen in the buffer before training
+ALPHA = .6  # default OpenAI
+BETA = .4  # default OpenAI
+PER_EPS = 1e-6
+
+
+# To visualize the policy, create a grid of the state space
+pos_grid = np.linspace(-1.2, .6, 16)
+speed_grid = np.linspace(-0.07, 0.07, 16)
+pos_speed_grid = np.array(list(product(pos_grid, speed_grid)))
+
+
+# OpenAI's environment
+env = gym.make('MountainCar-v0')
+
+
+# Neural networks and optimizer
+dqn = DQN(hdim=100)
+dqn_eval = deepcopy(dqn)
+dqn_eval.eval()
+lr = 2e-4
+momentum = .05
+optim = torch.optim.RMSprop(dqn.parameters(), lr=lr, momentum=momentum)  #torch.optim.Adam(dqn.parameters(), lr=lr)
+
+
+# Monitoring
+t = 0  # step counts
+successes = 0
+writer = SummaryWriter(log_dir=create_exp_dir())
+
+
+# Memory
+replay_memory = PrioritizedReplayBuffer(MAX_SIZE_BUFFER, ALPHA)
+
+
+# Misc
+observation_t = None
+loss = None
+wloss = None
+r = None
+
+
+# LEARN
+print('Learning')
+for n_episode in range(N_EPISODES):
+    print('episode %d/%d' % (n_episode, N_EPISODES))
+    print('Last final pos: %.2f\n' % observation_t[0]) if observation_t is not None else print()
+
+    # Init beginning of episode
+    observation_t = env.reset()
+    observation_tp1 = None
+    cum_reward = 0
+    done = False
+    t_ep = 0
+
+    while not done:
+        dqn.eval()
+
+        # Copy weights of eval network if necessary
+        update_eval_network(dqn_eval, dqn, t, TAU) if t > T_START_LEARNING else None
+
+        env.render()
+        t += 1
+        t_ep += 1
+        eps = .05 + .95*np.exp(-t*5 / N_RANDOM)
+
+        # OBSERVE
+        x = torch.from_numpy(observation_t)
+        observation_t = observation_tp1*1. if observation_tp1 is not None else observation_t
+
+        # TAKE ACTION
+        action = dqn.action(x, eps=eps).numpy()  # eps-greedy action selection
+        observation_tp1, _, done, _ = env.step(action)
+        pos = observation_tp1[0]*1.
+
+        # UPDATE REWARD (the reward provided by the environment is too sparse and does not help the algorithm to learn)
+        reward, successes = update_reward(pos, done, successes)
+        cum_reward += (GAMMA ** t_ep) * reward
+
+        # STORE IN MEMORY
+        replay_memory.add(observation_t, action, reward, observation_tp1, done)
+
+        # TRAIN
+        if len(replay_memory) > T_START_LEARNING:  # if enough saved transitions
+            if t % 3 == 0:  # TRAIN NOT TOO FREQUENTLY
+                dqn.train()
+
+                # Sample batch
+                s, a, r, s_, d, w, idx = replay_memory.sample(BATCH_SIZE, BETA, to_tensor=True)
+
+                # Compute loss
+                q = dqn.q_values(s, a)  # q(s_i, a_i) for all elements of the batch
+                target = build_target(dqn, dqn_eval, r, s_, d, GAMMA)
+                td_error = q - target
+                loss = huber(td_error)
+                wloss = torch.mean(w*loss)
+
+                # Update weights
+                optim.zero_grad()
+                wloss.backward()
+                optim.step()
+
+                # Update priorities
+                replay_memory.update_priorities(idx, torch.abs(td_error).detach().numpy() + PER_EPS)
+
+        # MONITORING
+        if done:
+            tensorboard(dqn, pos_speed_grid, writer, t, cum_reward, successes, t_ep, loss, wloss, r)
+            break
+
+env.close()
+
---
+++ ddqn_mountaincar_tf/utils.py
@@ -0,0 +1,173 @@
+import tensorflow as tf
+import numpy as np
+from matplotlib import pyplot as plt
+import os
+
+
+###########################################
+# NOT TESTED
+###########################################
+
+
+class DQN(tf.keras.Model):
+    """
+    3 layered fully-connected neural network with batch norm
+    It takes the state as inputs and outputs Q(s,0), Q(s,1), Q(s,2)
+    """
+    def __init__(self, hdim=100):
+        super(DQN, self).__init__()
+
+        # sequence of blocks FC + BN + ReLU
+        self.fc1 = tf.keras.layers.Dense(hdim, use_bias=False, activation='relu')  # 2d state space
+        self.bn1 = tf.keras.layers.BatchNormalization()
+        self.fc2 = tf.keras.layers.Dense(hdim, use_bias=False, activation='relu')
+        self.bn2 = tf.keras.layers.BatchNormalization()
+        self.fc3 = tf.keras.layers.Dense(3, use_bias=True)  # one output per action
+
+    def forward(self, x):
+        # reshape if necessary
+        xx = x*1. if len(x.shape) == 2 else tf.keras.layers.Reshape((-1, 1))(x)
+
+        # forward pass
+        xx = self.bn1(self.fc1(xx.float()))
+        xx = self.bn2(self.fc2(xx))
+        xx = self.fc3(xx)
+
+        # reshape if necessary
+        xx = xx if len(x.shape) == 2 else tf.keras.layers.Reshape(-1)(xx)
+        return xx
+
+    def action(self, x, return_score=False, eps=.1):
+        """
+        Choose action in epsilon greedy fashion
+        :param x:
+        :param eps: the probaility of selecting a random action
+        :return: an action, or one per element of the batch
+        """
+        values = self.forward(x)
+        u = np.random.random()
+        if u < eps:
+            values = np.random.random(values.shape)
+            return tf.argmax(values, axis=1 if len(x.shape) == 2 else 0)
+        else:
+            return tf.argmax(values, axis=1 if len(x.shape) == 2 else 0) if not return_score else (tf.argmax(values, axis=1 if len(x.shape) == 2 else 0), values)
+
+    # @todo: modify this one
+    def q_values(self, s, a):
+        return self.forward(s).gather(1, a).view(-1)
+
+
+# @todo: modify this one
+def build_target(dqn, dqn_eval, r, s_, d, gamma):
+    """Returns r + gamma * max_a { Q_eval(s_t+1,a) } """
+    dqn.eval()
+    dqn_eval.eval()
+
+    target = r.view(-1).float()
+
+    q = dqn.forward(s_)
+    greedy_actions = torch.max(q + torch.rand_like(q) / 1e6, 1)[1].view(-1, 1)   # actions that maximizes `Q(S_t+1, a)` w.r.t. `a`
+    update_target = dqn_eval.forward(s_).gather(1, greedy_actions).view(-1)  # values of these actions `a` with the evaluation network `Q'(S_t+1, a)`
+    update_target = (gamma * update_target[1 - d]).float()
+
+    target[1 - d] += update_target  # update only those transitions that are not done
+    target.detach_()  # don't propagate gradients through this
+
+    return target
+
+
+###########################################
+# TESTED
+###########################################
+
+# @todo: modify this one
+def update_eval_network(dqn_eval, dqn, i, tau):
+    """Copy the weights of the policy network into the target network"""
+    if i % tau == 0:
+        dqn_eval.load_state_dict(dqn.state_dict())
+        dqn_eval.eval()
+
+
+def create_exp_dir():
+    """Create a subdirectory within ./tensorboard to store the experiments with TensorBoard"""
+    i = 1
+    while True:
+        if os.path.isdir('./tensorboard/exp%d'%i):
+            i += 1
+        else:
+            break
+    os.mkdir('./tensorboard/exp%d'%i)
+    return 'tensorboard/exp%d'%i
+
+
+def huber(x):
+    """Huber function (~ smooth L1 norm of x)"""
+    mask = (tf.abs(x) < 1.).float()
+    return mask*.5*tf.pow(x, 2) + (1.-mask)*(tf.abs(x) - .5)
+
+
+def color(actions):
+    """
+    Color 0 in red (push left), 1 in yellow (do nothing), and 2 in green (push right)
+    The object of this function is to visualize the policy in the (position, speed) state space (to actually see whatthe policy looks like)
+    """
+    colors = []
+    for a in actions:
+        if a == 0:
+            colors.append('red')
+        elif a == 1:
+            colors.append('yellow')
+        else:
+            colors.append('green')
+    return colors
+
+
+def _reward(pos):
+    return np.exp(.1/(.6-pos))
+
+
+def update_reward(pos, done, successes):
+    """
+    Instead of using the sparse reward signal given by the environment Mountaincar-v0 (the episodes' rewards are always the same until you reach the flag once),
+     I use this signal that rewards getting closer to the flag, and penalize being to far, in particular staying in the middle
+    """
+    # reward = _reward(pos) - _reward(-.3)
+    reward = -1
+    if done:
+        if pos >= .5:
+            reward += 1  #50  # you want something big enough so that it is not worth chilling around on the rest of the hill to reap more rewards
+            successes += 1
+    return reward, successes
+
+
+def tensorboard(dqn, pos_speed_grid, writer, t, cum_reward, successes, t_ep, loss, wloss, r):
+    """Monitoring using TensorBoard"""
+    dqn.eval()
+    actions, q_scores = dqn.action(tf.Tensor(pos_speed_grid).float(), eps=0, return_score=True)
+    actions = actions.numpy().reshape(-1)
+    q_scores = q_scores.detach().numpy()
+
+    # policy
+    fig = plt.figure(1)
+    plt.clf()
+    plt.scatter(pos_speed_grid[:, 0], pos_speed_grid[:, 1], c=color(actions))
+    writer.add_figure('policy', fig, global_step=t)
+
+    # histograms of the scores, per action
+    fig = plt.figure(2)
+    plt.clf()
+    plt.hist(q_scores, bins=int(np.sqrt(q_scores.shape[0]))+1, color=('red', 'yellow', 'green'))
+    writer.add_figure('q_scores', fig, global_step=t)
+
+    # weights of the neural network
+    writer.add_histogram('fc1', dqn.fc1.weight.detach().numpy(), global_step=t)
+    writer.add_histogram('fc2', dqn.fc2.weight.detach().numpy(), global_step=t)
+    writer.add_histogram('fc3', dqn.fc3.weight.detach().numpy(), global_step=t)
+
+    # a few scalars to monitor the performance of the agent (cumulative rewards and loss essentially)
+    writer.add_scalar('num_successes_until_now', successes, global_step=t)
+    writer.add_scalar('cum_reward_in_episode', cum_reward, global_step=t)
+    writer.add_scalar('num_steps_in_episode', t_ep, global_step=t)
+    writer.add_scalar('loss', loss.mean().detach().numpy(), global_step=t) if loss is not None else None
+    writer.add_scalar('wloss', wloss.detach().numpy(), global_step=t) if wloss is not None else None
+    writer.add_scalar('nonzero_rewards_in_batch', (r.detach() > 0).float().sum().numpy(), global_step=t) if r is not None else None
